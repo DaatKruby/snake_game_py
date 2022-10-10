c12c0c81c1cbe13ac6c1718173c964886c46f132
DaatKruby <marcos.garcia@potros.itson.edu.mx>
2022-10-10 14:58:29 -0700

    added food to canvas

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from turtle import Screen, Turtle
+import turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
-screen.setup(width=1920, height=1080) #screen size ps si
-screen.bgcolor("black")
+screen.setup(width=600, height=600) #screen size ps si
+screen.bgcolor("grey")
 screen.tracer(0)               #avoid drawing
 screen.title("Culebrita game") #title mamalón
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.Up, "Up")
@@ -20,9 +23,12 @@ screen.onkey(snake.Right,"Right")
 game_is_on = True
 while game_is_on:
     screen.update()  #avoid running it from the start
-    time.sleep(.1)   #avoid running it by "fps" of user computer
+    time.sleep(.08)   #avoid running it by "fps" of user computer
 
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh();
 
 
 
